7c8df9dbc0db97e1dc001aad8554e9a74257c6db
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-10-07 10:14:31 -0300

    exercício 7 em python, readme

---
+++ 07 Preparação para HTML e CSS/01.py
@@ -0,0 +1,13 @@
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim',
+                 'Milton Nascimento', 'Chico Buarque', 'Nara Leão',
+                 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso',
+                 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa',
+                 'Gal Costa']
+
+
+quantidade_elementos = len(lista_musicos)
+
+print(f"A lista músicos possui: {quantidade_elementos} elementos")
+print("Dado salvo no índice 2: ", lista_musicos[2])
+print("Dado salvo no índice 9: ", lista_musicos[9])
+print("Dado salvo no índice 14: ", lista_musicos[14])
