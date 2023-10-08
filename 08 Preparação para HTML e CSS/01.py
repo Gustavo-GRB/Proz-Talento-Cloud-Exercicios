6800db85cb2cc3ffc2e198210e2d22a3479c1bc9
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-10-08 11:57:41 -0300

    exercício 8 em python, readme

---
+++ 08 Preparação para HTML e CSS/01.py
@@ -0,0 +1,5 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes',
+                  'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+for elemento in lista_produtos:
+    print(f"Temos {elemento} à venda!")
