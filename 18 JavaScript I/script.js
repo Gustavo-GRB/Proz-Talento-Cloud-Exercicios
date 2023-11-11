1f8f53e5868ce0a2ed902eb0e8e28bfba3264f18
gustavo.rocha <gustavorocha.net@hotmail.com>
2023-11-10 23:04:24 -0300

    adicionado o defer no index.html, alterado ordem no script.js

--- 18 JavaScript I/script.js
+++ 18 JavaScript I/script.js
@@ -1,8 +1,7 @@
+function mensagem() {
+    return 'Conexão feita com sucesso!'
+}
 
 for (let i = 0; i < 3; i++) {
     console.log(mensagem());
-}
-
-function mensagem() {
-    return 'Conexão feita com sucesso!'
 }
