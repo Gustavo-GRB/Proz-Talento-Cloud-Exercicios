bd6a389dfe6efad3545def409f1c0b7697f27524
gustavo.rocha <gustavorocha.net@hotmail.com>
2024-01-20 18:16:51 -0300

    Adicionando exercício na pasta 20

---
+++ 20 JavaScript I/script.js
@@ -0,0 +1,9 @@
+let numA = 4;
+let numB = 5;
+
+let operacao = `${numA} + ${numB} = ${numA + numB}
+${numA} - ${numB} = ${numA - numB}
+${numA} x ${numB} = ${numA * numB}
+${numA} / ${numB} = ${numA / numB}
+`
+console.log(operacao);
